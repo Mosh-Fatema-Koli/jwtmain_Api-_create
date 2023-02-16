1467ab1fa53d87f59107d8fa896b240c2b15f083
Mosh-Fatema-Koli <fatemakoli58@gnail.com>
2023-02-16 14:11:33 +0600

    api added

--- router.js
+++ router.js
@@ -58,6 +58,7 @@ router.post('/register', signupValidation, (req, res, next) => {
     );
 });
 
+
 router.post('/login', loginValidation, (req, res, next) => {
     db.query(
         `SELECT * FROM users WHERE email = ${db.escape(req.body.email)};`,
@@ -106,6 +107,61 @@ router.post('/login', loginValidation, (req, res, next) => {
     );
 });
 
+
+
+router.get('/users', function (req, res) {
+
+    db.query('SELECT * FROM users', function (error, results, fields) {
+    if (error) throw error;
+    return res.send({ error: false, data: results, message: 'users list.' });
+    });
+
+    });
+
+
+
+    
+// Retrieve user with id 
+router.get('/users/:id', function (req, res) {
+    let user_id = req.params.id;
+    if (!user_id) {
+    return res.status(400).send({ error: true, message: 'Please provide user_id' });
+    }
+    db.query('SELECT * FROM users where id=?', user_id, function (error, results, fields) {
+    if (error) throw error;
+    return res.send({ error: false, data: results[0], message: 'users list.' });
+    });
+    });
+
+
+    //  Update user with id
+router.put('/users/update', function (req, res) {
+    let user_id = req.body.user_id;
+    let user = req.body.user;
+    if (!user_id || !user) {
+    return res.status(400).send({ error: user, message: 'Please provide user and user_id' });
+    }
+    db.query("UPDATE users SET user = ? WHERE id = ?", [user, user_id], function (error, results, fields) {
+    if (error) throw error;
+    return res.send({ error: false, data: results, message: 'user has been updated successfully.' });
+    });
+    });
+
+
+    //  Delete user
+router.delete('/user', function (req, res) {
+
+    let user_id = req.body.user_id;
+    if (!user_id) {
+        return res.status(400).send({ error: true, message: 'Please provide user_id' });
+    }
+
+    db.query('DELETE FROM users WHERE id = ?', [user_id], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'User has been updated successfully.' });
+    });
+    }); 
+/*
 router.get('/users', signupValidation, (req, res, next)=>{
 
     if(
@@ -127,9 +183,9 @@ const decoded = jwt.verify(theToken, 'the-super-strong-secrect');
 
     });
 });
+*/
 
-
-router.post('/get-user', signupValidation, (req, res, next) => {
+router.get('/profile', signupValidation, (req, res, next) => {
     if(
         !req.headers.authorization ||
         !req.headers.authorization.startsWith('Bearer') ||
